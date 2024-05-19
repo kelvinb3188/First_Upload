b8645a8a952d0d3be5a023ee5863d2312034fb98
Kelvin B Taylor <kelvinbbrown@mavs.uta.edu>
2024-05-19 15:56:45 -0500

    Create RoollerCoasterHeightCheck_For_GitHub.py

---
+++ RoollerCoasterHeightCheck_For_GitHub.py
@@ -0,0 +1,6 @@
+#RoollerCoasterHeightCheck
+height = int(input('Enter the riders height (in inches): '))
+if height >= 48:
+    print('Congraulations, you are tall enough to ride the rollercoaster.')
+else:
+    print('Sorry, you are not yet tall enough to get on this ride. Please try another ride in the appropriate area.')
